518afad7090e29c1ef3a038898d9774afabcb951
Seung-hwan285 <ghks123285@gmail.com>
2023-11-04 20:46:25 +0900

    chore : test

--- src/components/product/ProductLogo.tsx
+++ src/components/product/ProductLogo.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 import styles from "@/app/[lang]/page.module.css";
-
-import Image from "next/image";
-
-import Icon from "/public/home.svg";
-
 import ImageWithPlaceholder from "@/components/commons/ImageMetaData";
 
 function ProductLogo() {
