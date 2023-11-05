b68a918eaa02e08aceb3ed8d4d8029e583768a04
Seung-hwan285 <ghks123285@gmail.com>
2023-11-05 20:13:50 +0900

    refactor : Add edge runtime

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  runtime: "edge",
   experimental: {
     appDir: true,
     serverActions: true,
-    serverActionsBodySizeLimit: '2mb',
+    serverActionsBodySizeLimit: "2mb",
   },
   typescript: {
     ignoreBuildErrors: true,
   },
 
   images: {
-    domains: ['lh3.googleusercontent.com', 'cdn.chec.io'],
+    domains: ["lh3.googleusercontent.com", "cdn.chec.io"],
   },
 };
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNextIntl = require('next-intl/plugin')(
+const withNextIntl = require("next-intl/plugin")(
   // This is the default (also the `src` folder is supported out of the box)
-  './i18n.ts'
+  "./i18n.ts"
 );
 
 module.exports = withNextIntl;
--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { isRoutesError } from '@/lib/type-guards/isRoutesError';
-
+export const runtime = 'edge';
 function formatErrorMessage(err: Error): string {
   // 모든 키 반환
   return JSON.stringify(err, Object.getOwnPropertyNames(err));
