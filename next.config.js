0cf35387e61ba894ee78d0b4a84f0e3aa4a3ba2c
Seung-hwan285 <ghks123285@gmail.com>
2023-11-07 10:31:08 +0900

    chore: test

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  pageExtensions: ["ts", "tsx", "svg"],
+  pageExtensions: ['ts', 'tsx', 'svg'],
   experimental: {
     mdxRs: true,
     appDir: true,
     serverActions: true,
-    serverActionsBodySizeLimit: "2mb",
+    serverActionsBodySizeLimit: '2mb',
   },
 
+  // test
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".svg"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.svg'],
   },
 
   typescript: {
@@ -18,18 +19,18 @@ const nextConfig = {
   },
 
   images: {
-    domains: ["lh3.googleusercontent.com", "cdn.chec.io"],
+    domains: ['lh3.googleusercontent.com', 'cdn.chec.io'],
     minimumCacheTTL: 60,
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [576, 640, 750, 1080, 1200, 1920],
-    formats: ["image/avif", "image/webp"],
+    formats: ['image/avif', 'image/webp'],
   },
-  testEnvironment: "node",
+  testEnvironment: 'node',
 };
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 });
 
 module.exports = nextConfig;
